23cf279218c3e2aef8948ea5cefda42c80d89971
anupam yadav <145851264+Anupamyadav7428@users.noreply.github.com>
2025-08-17 14:11:21 +0530

    ready for render deploy

--- django-html-chat/backchat/settings.py
+++ django-html-chat/backchat/settings.py
@@ -65,12 +65,16 @@ TEMPLATES = [
 WSGI_APPLICATION = "backchat.wsgi.application"
 ASGI_APPLICATION = "backchat.asgi.application"
 
+import dj_database_url
+import os
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}"
+    )
 }
+
+
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
